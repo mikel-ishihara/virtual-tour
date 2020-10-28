de500cfcd592f53504707ec24b5c03bf5e76d85d
mikel <mikel123@hawaii.rr.com>
2020-10-27 19:20:15 -1000

    t

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -13,11 +13,12 @@ class NavBar extends React.Component {
     return (
       <Menu style={menuStyle} attached="top" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
-          <Header inverted as='h1'>meteor-application-template</Header>
+          <Header inverted as='h1'>UH Manoa Virtual Tour</Header>
         </Menu.Item>
+        <Menu.Item as={NavLink} activeClassName="active" exact to="/locations" key="locations">Locations</Menu.Item>
         {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
+            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Locations</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Locations</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
---
+++ app/imports/ui/pages/Locations.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+
+class Locations extends React.components {
+  render() {
+    return (
+        <Grid>
+          <Grid.Column width={4}>
+            <h1>Welcome to this template</h1>
+            <p>Now get to work and modify this app!</p>
+          </Grid.Column>
+        </Grid>
+    );
+  }
+}
+
+export default Locations;
