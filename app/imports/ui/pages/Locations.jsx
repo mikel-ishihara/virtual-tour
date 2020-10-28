dd4ad0118812e7c65f224897abbf763e3b26d827
mikel <mikel123@hawaii.rr.com>
2020-10-27 21:31:10 -1000

    unfinished

--- app/imports/ui/pages/Locations.jsx
+++ app/imports/ui/pages/Locations.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Card, Image } from 'semantic-ui-react';
 
-class Locations extends React.components {
+class Locations extends React.Component {
   render() {
     return (
         <Grid>
           <Grid.Column width={4}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+            <Card>
+              <Image src='https://www.staradvertiser.com/wp-content/uploads/2017/01/web1_CTY-UH-Explosion-Report-084.jpg'/>
+              <Card.Content>
+                <Card.Header>POST</Card.Header>
+                <Card.Description>POST is located next to Holmes Hall</Card.Description>
+              </Card.Content>
+            </Card>
           </Grid.Column>
         </Grid>
     );
---
+++ app/imports/ui/pages/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+
+class POST extends React.Component {
+  render() {
+    return (
+      <Image src='https://goo.gl/maps/2GtSa43RYLpDzMgR9'/>
+    );
+  }
+}
+
+export default POST;
